6fc229bcf12ebe0ad4f3e2823c52b753caf321e2
Thea Clarisse P. Delos Santos <theadelossantos08@gmail.com>
2023-10-31 18:57:18 +0800

    update

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -15,8 +15,11 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    console.log('AuthGuard: Checking authentication status...');
+    console.log('Is Authenticated:', this.authService.isAuthenticated());
 
     if (!this.authService.isAuthenticated()) {
+      console.log('AuthGuard: Access denied - Redirecting to login');
       this.router.navigate(['/']);
       return false;
     }
@@ -24,14 +27,20 @@ export class AuthGuard implements CanActivate {
     const requiredRoles = next.data['roles'] || [];
     const userRoles = this.authService.getUserRoles();
 
+    console.log('AuthGuard: Allowed Roles:', requiredRoles);
+    console.log('AuthGuard: User Roles:', userRoles);
+
     const hasRequiredRole = requiredRoles.some(role => userRoles.includes(role));
 
     if (hasRequiredRole) {
+      console.log('AuthGuard: Access granted');
       return true;
     } else {
       if (state.url === '/admin') {
+        console.log('AuthGuard: Access granted for /admin route');
         return true;
       }
+      console.log('AuthGuard: Access denied - Redirecting to login');
       this.router.navigate(['/admin']);
       return false;
     }
