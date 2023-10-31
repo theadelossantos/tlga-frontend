793cb965b0b1934955faa624c339cfff8cc5f2d4
Thea Clarisse P. Delos Santos <theadelossantos08@gmail.com>
2023-10-31 10:15:03 +0800

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: cd backend && python manage.py runserver 0.0.0.0:$PORT
+web: node server.js
+web: python ./.backend/manage.py runserver 0.0.0.0:$PORT
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+1brdjw4y*sawub6uxt2qye#t91=4bt311lb4=9p_$m793y%h%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
